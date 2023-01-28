4bfedcbbbca906be9ea79f336de56d64260968fa
Jhonatan1022780 <rojasjhonatan324@gmail.com>
2023-01-28 18:30:50 -0500

    add animations

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,15 +2,20 @@ import { Board } from '../components/Board'
 import { Header } from '../components/Header'
 import { ModalWinner } from '../components/Modals/ModalWinner/ModalWinner'
 import { Layout } from '../layout/Layout'
+import { useStore } from '../store/store'
 
 export const Game = () => {
+  const { player } = useStore(state => state)
+
   return (
     <Layout>
       <div className='flex flex-col justify-center h-full'>
         <Header />
         <Board />
       </div>
-      <ModalWinner />
+      {
+        player.winner && <ModalWinner />
+      }
     </Layout>
   )
 }
